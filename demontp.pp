f986bf20511cf7ea5ccf20ff9a99483ab8941097
Imaim-vilela-2compc <imaim.vilela@2comconsulting.com.br>
2022-11-18 17:19:58 -0300

    test

--- demontp.pp
+++ demontp.pp
@@ -6,7 +6,7 @@ package { "ntp":
 # Arquivo de configuração
 file { "/etc;ntp.conf":
     ensure => "present",
-    content => "server a.st1.ntp.br iburst nts\n",
+    content => "server server a.ntp.br\n server b.ntp.br\n server c.ntp.br iburst nts\n",
 }
 
 # Start no serviço do ntp
